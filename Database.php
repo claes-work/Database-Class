8976ec4446b8670d8d57fd58f71ca95087f2d11b
s.claes <Sadrian2711>
2021-09-18 08:32:44 +0200

    [NEW] created the Database.php constructor method

--- Database.php
+++ Database.php
@@ -11,4 +11,29 @@ class Database {
     protected $query_closed = TRUE;
     public    $query_count  = 0;
 
+
+    /**
+     * Database constructor
+     *
+     * @param string $dbHost
+     * @param string $dbUser
+     * @param string $dbPass
+     * @param string $dbName
+     * @param string $charset
+     */
+    public function __construct(
+        $dbHost  = 'localhost',
+        $dbUser  = 'root',
+        $dbPass  = '',
+        $dbName  = '',
+        $charset = 'utf8'
+    ) {
+        // create new database object
+        $this->connection = new mysqli($dbHost, $dbUser, $dbPass, $dbName);
+        // check if there is a connection error
+        ($this->connection->connect_error) ?: $this->error('Failed to connect to MySQL - ' . $this->connection->connect_error);
+        // set the msqli charset
+        $this->connection->set_charset($charset);
+    }
+
 }
