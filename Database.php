2368f322e502340f751cf48bce7f04d16bbc58c0
s.claes <Sadrian2711>
2021-09-18 08:31:50 +0200

    [NEW] created all Database.php class properties

--- Database.php
+++ Database.php
@@ -2,6 +2,13 @@
 
 class Database {
 
-
+    /**
+     * Database class properties
+     */
+    protected $connection;
+    protected $query;
+    protected $show_errors  = TRUE;
+    protected $query_closed = TRUE;
+    public    $query_count  = 0;
 
 }
